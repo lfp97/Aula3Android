351b5a33c9febdf060f29298ff8d797b6a3dd5b3
Lucas <lucaspaim97@hotmail.com>
2018-04-08 20:48:58 -0300

    Startando Activity, consertando intent.getFloatExtra, app finalizado.

--- app/src/main/java/com/example/lucas/notas/Main2Activity.kt
+++ app/src/main/java/com/example/lucas/notas/Main2Activity.kt
@@ -3,6 +3,7 @@ package com.example.lucas.notas
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class Main2Activity : AppCompatActivity() {
@@ -19,14 +20,15 @@ class Main2Activity : AppCompatActivity() {
 
     fun onClickCalcularFinal (view: View)
     {
-        val mediaAntiga = intent.getIntExtra(TOTAL_MEDIA, 0)
-        val notaFinal= findViewById<View>(R.id.editTextNotaFinal)
-        val notaFinalComPeso= notaFinal.toString().toFloat() *3
+        val mediaAntiga = intent.getFloatExtra(TOTAL_MEDIA, 0F)
+        val notaFinal= findViewById<View>(R.id.editTextNotaFinal) as EditText
+        val notaFinalComPeso= notaFinal.text.toString().toFloat() *3
         val mediaNova= mediaAntiga + notaFinalComPeso
+        //Toast.makeText(applicationContext, "Media Antiga= " + mediaAntiga + " Nova Media= " + mediaNova, Toast.LENGTH_LONG).show()
 
         if (mediaNova >= 50)
-            Toast.makeText(applicationContext, "Aluno aprovado! Media= " + mediaNova, Toast.LENGTH_LONG).show()
+          Toast.makeText(applicationContext, "Aluno aprovado! Media= " + mediaNova, Toast.LENGTH_LONG).show()
         else
-            Toast.makeText(applicationContext, "Aluno reprovado! Media= " + mediaNova, Toast.LENGTH_LONG).show()
+          Toast.makeText(applicationContext, "Aluno reprovado! Media= " + mediaNova, Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/lucas/notas/MainActivity.kt
+++ app/src/main/java/com/example/lucas/notas/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,18 +16,20 @@ class MainActivity : AppCompatActivity() {
 
     fun onClickCalcular (view: View)
     {
-        val editTextNota1= findViewById<View>(R.id.editTextPrimeiraNota)
-        val nota1ComPeso= editTextNota1.toString().toFloat() *3
+        val editTextNota1= findViewById<View>(R.id.editTextPrimeiraNota) as EditText
+        val nota1ComPeso= editTextNota1.text.toString().toFloat() *3
 
-        val editTextNota2= findViewById<View>(R.id.editTextSegundaNota)
-        val nota2ComPeso= editTextNota2.toString().toFloat() *4
+        val editTextNota2= findViewById<View>(R.id.editTextSegundaNota) as EditText
+        val nota2ComPeso= editTextNota2.text.toString().toFloat() *4
 
         val media= nota1ComPeso + nota2ComPeso
+        //Toast.makeText(applicationContext, "Media= " + media, Toast.LENGTH_LONG).show()
 
         if (media >= 29 && media < 49)
         {
             val notaIntent= Intent(this, Main2Activity::class.java)
             notaIntent.putExtra(Main2Activity.TOTAL_MEDIA, media)
+            startActivity(notaIntent)
         }
         if (media >= 49) //aprovado
             Toast.makeText(applicationContext, "Aluno aprovado! Media= " + media, Toast.LENGTH_LONG).show()
